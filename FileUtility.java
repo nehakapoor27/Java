c9e488a87ba34c35cf67850fe49c05c13b2fc441
nehakapoor27 <nehakapoor27@gmail.com>
2016-12-06 16:09:18 -0800

    Add files via upload

--- FileUtility.java
+++ FileUtility.java
@@ -2,33 +2,47 @@ package com.foo;
 
 import java.io.File;
 
-
 public class FileUtility {
-	
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-	File file = new File("C:\\Software");
-	
-	System.out.println(file.getParent());
-	System.out.println(file.isDirectory());
-	System.out.println(file.isFile());
-	System.out.println(file.isHidden());
-	System.out.println(file.lastModified());
-	
-	String [] filename = file.list();
-	// length = filename.length;
-	for (int i=0; i<filename.length;i++){
-		String name = filename[i];
-		System.out.println(name);
-		
-	}
-	
-	//System.out.println(file.list());
-		
+		File file = new File("C:\\Software");
+
+		System.out.println(file.getParent());
+		System.out.println(file.isDirectory());
+		System.out.println(file.isFile());
+		System.out.println(file.isHidden());
+		// System.out.println(file.lastModified());
+
+		String[] filename = file.list();
+		// length = filename.length;
+		for (int i = 0; i < filename.length; i++) {
+			String name = filename[i];
+			System.out.println(name);
+
+		}
+
+		File[] obj = file.listFiles();
+		for (int i = 0; i < obj.length; i++) {
+
+			File f = obj[i];
+			System.out.println(f);
+			String name = f.getName();
+			System.out.println(name);
+
+			boolean isDirectory = f.isDirectory();
+			if (isDirectory) {
+
+				System.out.println(name + "" + "Directory");
+
+			}
+			else {
+				
+				System.out.println(name + "" + "File");
+			}
+		}
+
 	}
 
 }
