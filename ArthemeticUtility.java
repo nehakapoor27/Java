6f977e79413d37e500c72f90203aac0b04644895
nehakapoor27 <nehakapoor27@gmail.com>
2016-12-06 16:53:28 -0800

    Add files via upload

---
+++ ArthemeticUtility.java
@@ -0,0 +1,22 @@
+package com.foo;
+
+public class ArthemeticUtility {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 5;
+		int y = 0;
+		try {
+		System.out.println(x/y);
+		}
+		catch (Exception e){
+			System.out.println("Please check your input");
+			
+		}
+
+	}
+
+}
